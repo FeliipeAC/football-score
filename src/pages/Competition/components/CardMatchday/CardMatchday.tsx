aed0d7feb9bcbc16066a68d81b5fe3dfb4ef87c7
Felipe Andrade <felipe_fac_8@hotmail.com>
2022-07-04 21:17:42 -0300

    Tratamento da logo dos times na lista de jogos da competição

--- src/pages/Competition/components/CardMatchday/CardMatchday.tsx
+++ src/pages/Competition/components/CardMatchday/CardMatchday.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Popover } from "antd";
 import moment from "moment";
+import { TeamShield } from "../../../../components/CardMatch/CardMatchStyle";
 import { Match } from "../../../../models/match.model";
 import {
 	CardMatchDayContainer,
@@ -9,7 +10,15 @@ import {
 	TeamLink,
 } from "./CardMatchdayStyle";
 
+import ShiledIconPlaceholder from "../../../../assets/images/shield_icon.png";
+
 export function CardMatchDay({ matchday }: { matchday: Match }) {
+	const handleOnErrorAvatar = () => {
+		console.log("error");
+		const img = new Image();
+
+		return true;
+	};
 	return (
 		<>
 			<CardMatchDayContainer>
@@ -19,11 +28,19 @@ export function CardMatchDay({ matchday }: { matchday: Match }) {
 				<ScoreContainer>
 					<Popover content={matchday.homeTeam.name} trigger="hover">
 						<TeamLink to={`/teams/${matchday.homeTeam.id}`}>
-							<Avatar
-								shape="square"
-								size="default"
+							<TeamShield
 								src={`https://crests.football-data.org/${matchday.homeTeam.id}.png`}
-							/>
+								onError={({ currentTarget }) => {
+									currentTarget.onerror = null;
+									if (currentTarget.src.includes(".png")) {
+										currentTarget.src = `https://crests.football-data.org/${matchday.homeTeam.id}.svg`;
+									} else {
+										currentTarget.src =
+											ShiledIconPlaceholder;
+									}
+								}}
+								alt={matchday.homeTeam.name}
+							></TeamShield>
 						</TeamLink>
 					</Popover>
 					<Score>
@@ -33,11 +50,19 @@ export function CardMatchDay({ matchday }: { matchday: Match }) {
 					</Score>
 					<Popover content={matchday.awayTeam.name} trigger="hover">
 						<TeamLink to={`/teams/${matchday.awayTeam.id}`}>
-							<Avatar
-								shape="square"
-								size="default"
+							<TeamShield
 								src={`https://crests.football-data.org/${matchday.awayTeam.id}.png`}
-							/>
+								onError={({ currentTarget }) => {
+									currentTarget.onerror = null;
+									if (currentTarget.src.includes(".png")) {
+										currentTarget.src = `https://crests.football-data.org/${matchday.awayTeam.id}.svg`;
+									} else {
+										currentTarget.src =
+											ShiledIconPlaceholder;
+									}
+								}}
+								alt={matchday.awayTeam.name}
+							></TeamShield>
 						</TeamLink>
 					</Popover>
 				</ScoreContainer>
